fb8e205e5902f622b76a3e05621e4f9db3952965
Martin Hochel <hochelmartin@gmail.com>
2018-08-14 10:04:16 +0200

    chore(examples): add state type to root app

--- examples/counter/src/app/app.tsx
+++ examples/counter/src/app/app.tsx
@@ -3,14 +3,13 @@ import { Component, createElement } from 'react'
 
 import { Counter } from './counter'
 import { configureStore } from './store.config'
-
 import { Actions } from './store/actions'
 
 const store = configureStore()
 const { dispatch } = store
 
-export class App extends Component {
-  state = {}
+export class App extends Component<{}, ReturnType<typeof store.getState>> {
+  state = { counter: 0 }
   render() {
     return (
       <div className="app">
