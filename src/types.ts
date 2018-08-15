f549b16292829528f7bbf766ff4aba287b096058
Martin Hochel <hochelmartin@gmail.com>
2018-08-15 17:21:03 +0200

    feat(types): add constructor and omit

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,4 @@
 export type AnyFunction = (...args: any[]) => any
 export type StringMap<T> = { [key: string]: T }
+export type Constructor<T = {}> = new (...args: any[]) => T
+export type Omit<T, K> = Pick<T, Exclude<keyof T, keyof K>>
