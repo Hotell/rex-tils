c89f36f09cff4a5de9354d8bd7891f2c7eefa6b2
Martin Hochel <hochelmartin@gmail.com>
2018-10-15 15:53:08 +0200

    feat(utils): add Enum helper for creating type safe enums

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,26 @@
+import { UnionFromTuple } from '../types'
+
 // tslint:disable-next-line:no-empty
 export const noop = () => {}
 export const identity = <T>(arg: T) => arg
+
+/**
+ * Enum function to create type safe immutable object map with runtime presence
+ * @param args
+ * @example
+ * ```ts
+ * // $ExpectType Readonly<{ No: "No"; Yes: "Yes"; }>
+ * const AnswerResponse = Enum('No', 'Yes')
+ *
+ * // $ExpectType Readonly<{ RED: "RED"; GREEN: "GREEN"; BLUE: "BLUE"; }>
+ * const Colors = Enum('RED', 'GREEN', 'BLUE')
+ * ```
+ */
+export const Enum = <T extends string[]>(...args: T) => {
+  return Object.freeze(args.reduce((acc, next) => {
+    return {
+      ...acc,
+      [next]: next,
+    }
+  }, Object.create(null)) as { [P in UnionFromTuple<typeof args>]: P })
+}
