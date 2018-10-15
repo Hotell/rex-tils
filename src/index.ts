e77b17c7249a8f73da1c703967e0fbe9d8a2195e
Martin Hochel <hochelmartin@gmail.com>
2018-10-15 16:53:31 +0200

    build(tsc): add ambient header to root index with supported TS version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+// Type definitions for @martin_hotell/rex-tils 2.0
+// Project: https://github.com/Hotell/rex-tils
+// Definitions by: Martin Hochel <https://github.com/hotell>
+// Definitions: https://github.com/Hotell/rex-tils
+// TypeScript Version: 3.1
+
 /**
  * React/Preact utils
  */
